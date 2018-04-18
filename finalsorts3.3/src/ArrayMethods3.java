b611a046a77090b3b1c91e8fb0f5621f898ba2cb
owner <owner@192.168.2.30>
2018-04-17 23:23:59 -0400

    .

--- finalsorts3.3/src/ArrayMethods3.java
+++ finalsorts3.3/src/ArrayMethods3.java
@@ -3,7 +3,16 @@ public class ArrayMethods3 {
 	
 	public static String[] mergeSort(String[] list)
 	{
-		
+		if (list.length == 1)
+		{
+			return list;
+		}
+		else 
+		{
+			//copyofRange() creates copy of list from specified range
+			String [] left = 
+			String [] right = list[list.length/2 , list.length - 1]
+		}
 	}
 	
 	public static int partition(int[] list1, int front, int back)
