b6fc27e01620612f00d92971ee997bb71c24fbfd
owner <owner@192.168.2.30>
2018-04-18 01:15:13 -0400

    .

--- finalsorts3.3/src/ArrayMethods3.java
+++ finalsorts3.3/src/ArrayMethods3.java
@@ -1,6 +1,47 @@
 import java.util.Arrays;
 
-public class ArrayMethods3 {
+public class ArrayMethods3 
+{
+
+	public static void main(String[] args)
+	{
+		// testing arrays
+		
+		String[] test1 = new String[] {"banana", "cherry", "mahogany", "oreos", "pinata"};
+		int[] test2 = {2, 5, 1, 7, 3, 9, 2};
+		void test3 = {6, 1, 2, 7, 5, 1, 4};
+		
+		
+		// MergeSort Test 
+		
+		long start = System.nanoTime();
+		String[] mergeResult = mergeSort(test1);
+		long end = System.nanoTime();
+		long time = end - start;
+		System.out.println("Merge test took: " + time + "nanoseconds");
+		System.out.println(Arrays.toString(mergeResult)); 
+		
+		//PartitionSort Test 
+		start = System.nanoTime();
+		int pivotFinalPos = partitionSort(test2, 0, 0);
+		end = System.nanoTime();
+		time = end - start;
+		System.out.println("PartitionSort test took: " + time + "nanoseconds");
+		System.out.println("Final Pivot Position: " + pivotFinalPos);  
+		System.out.println(Arrays.toString(test2)); 
+		
+		
+		//PartitionSort Test 
+		start = System.nanoTime();
+		int pivotFinalPos = quickSort(test3, 0, test3.length - 1);
+		end = System.nanoTime();
+		time = end - start;
+		System.out.println("PartitionSort test took: " + time + "nanoseconds");
+		System.out.println("Final Pivot Position: " + pivotFinalPos);  
+		System.out.println(Arrays.toString(test2));
+						
+	}
+	
 	
 	public static String[] mergeSort(String[] list)
 	{
@@ -83,6 +124,13 @@ public class ArrayMethods3 {
 	
 	public static void quickSort(int[] list1, int front, int back)
 	{
-		
+		if (front < back)
+		{
+			int pivotPos = partitionSort(list1, front, back)
+			
+			quickSort(list1, front, pivotPos - 1);
+			quickSort(list1, pivotPos + 1, back);
+			
+		}
 	}
 }
